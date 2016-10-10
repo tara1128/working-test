5f1593df7908a3b61ec7045293d4ae35a6dab172
Alex <tara1128@126.com>
2016-10-10 19:09:32 +0800

    Add animations for wtc.

--- whatscall/js/whatscall.js
+++ whatscall/js/whatscall.js
@@ -1,17 +1,42 @@
 /*
   whatscall.js
-  Latest modified: 2016-10-10 11:45
+  Latest modified: 2016-10-10 19:04
 */
 
 (function(){ 
 
-/* Animation of video button hover: */
-var vdBtn = $('#wtcVdBtn');
-var vdhrt = $('#wtcVdHeart');
-vdBtn.mouseenter(function(){
-  
-});
+  /* Initial Skroolr: */
 
+  /* Cache variables for elements: */
+  var vdBtn = $('#wtcVdBtn');
+  var vdhrt = $('#wtcVdHeart');
+  var trulyFree = $('#wtcTrulyFree');
+  var callCredits = $('#wtcCallCredits');
+  var noInternet = $('#wtcNoInternet');
+  var harassment = $('#wtcHarassment');
+
+  /* Animation of video button hover: */
+  vdBtn.mouseenter(function(){
+  });
+
+  /* Listen ScrollBar: */
+  $(window).scroll(function(){
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var clitWidth = document.body.clientWidth;
+    console.log('Scrolling: top =', scrollTop );
+    if( scrollTop > 380 ){
+      trulyFree.addClass('active');
+    }
+    if( scrollTop > 900 ){
+      callCredits.addClass('active');
+    }
+    if( scrollTop > 1500 ){
+      noInternet.addClass('active');
+    }
+    if( scrollTop > 2000 ){
+      harassment.addClass('active');
+    }
+  });
 
 
 
