c3248b01fceb25ce9c1c2528da3f901fa81a2dfd
Alex <tara1128@126.com>
2016-11-03 00:52:03 +0800

    Update webpack for i18n

--- test-i18n/src/pages/cms/webpack.config.js
+++ test-i18n/src/pages/cms/webpack.config.js
@@ -1,50 +1,57 @@
 /*
   Webpack configuration file for CMS
-  Latest modified: 2016-11-02 18:15
+  Latest modified: 2016-11-03 00:35
 */
 
 var path = require('path');
 var findRoot = require('find-root');
-var devRootPath = findRoot();
-var projectRootName = path.basename(path.resolve());
 var HtmlWebpackPlugin = require('html-webpack-plugin'); 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var config = require('./config');
 
-var distPath = devRootPath + '/dist/'; /* Identical to the root of www.cmcm.com/ */
-var cssPath = distPath + '/css/';
-var jsPath = distPath + '/js/';
-var imgPath = distPath + '/images/';
+var devRootPath = findRoot(); // src
+// How to get the upper dir of src?
+var projectRootName = path.basename(path.resolve());
 
-// var sourceJson
+/* Set paths for the distribution: */
+var distPath = devRootPath + '/' + config.websiteRoot; 
+var cssPath = distPath + '/' + config.styleRoot;
+var jsPath = distPath + '/' + config.scriptRoot;
+var imgPath = distPath + '/' + config.imagesRoot;
 
-/* Get all languages this project supported: */
-var langs = require('../../i18n/cms/langs.json').langs;
+/* Path of multi-language json files: */
+var jsonPath = devRootPath + '/i18n/' + projectRootName + '/';
 
-/* Variables for final index page: */
+/* All languages this project supported: */
+var langs = require( jsonPath + 'langs.json').langs;
+
+/* Properties for the final index page: */
 var htmlDefaultOptions = {
-  template: projectRootName + '.hbs',
+  template: projectRootName + '.hbs', // Need header&footer
   inject: 'body'
 };
 
-/* Make the array for 'plugins', with the traversal of langs: */
+/* Traversal of langs, creating plugins array */
 var pluginsArray = [];
 langs.map(function( lang, index ){
   var _mergedOptions = {};
-  var _htmlOptions = require('../../i18n/' + projectRootName + '/' + lang + '.json' );
-  var _fileNameObj = {filename: '../../../dist/' + lang + '/' + projectRootName + '/index.html'};
+  var _htmlOptions = require( jsonPath + lang + '.json' );
+  var _fileNameObj = {filename: distPath + '/' + lang + '/' + projectRootName + '/index.html'};
   Object.assign(_mergedOptions, htmlDefaultOptions, _fileNameObj, _htmlOptions);
   var _htmlWebpackPlugin = new HtmlWebpackPlugin(_mergedOptions);
   pluginsArray.push( _htmlWebpackPlugin );
 });
-pluginsArray.push(new ExtractTextPlugin( projectRootName + '.min.css'));
+
+/* After traversal, add CSS */
+pluginsArray.push(new ExtractTextPlugin( cssPath + '/' + projectRootName + '.min.css'));
 
 /* Here comes the module to export! */
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, '../../../dist/assets/' + projectRootName + '/' ),
+    path: jsPath,
     publicPath: '',
-    filename: './assets/' + projectRootName + '.min.js'
+    filename: projectRootName + '.min.js'
   },
   module: {
     loaders: [
@@ -56,3 +63,4 @@ module.exports = {
   },
   plugins: pluginsArray
 };
+
