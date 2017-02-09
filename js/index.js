45a8e1032f94111335ea5b8e9a6eea478333eee2
wangshaojing <wangshaojing@cmcm.com>
2017-02-09 16:16:26 +0800

    Remove alt for performance ads image

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*
   index.js, trying ES6
-  Latest modified: 2017-02-09 15:35
+  Latest modified: 2017-02-09 16:15
 */
 
 class IndexLogin extends React.Component {
@@ -244,7 +244,7 @@ var cateList = [
   {
     projectName: 'Cheetah Ads Platform',
     linkTo: 'ads/',
-    latestModified: '2017-02-09 15:52'
+    latestModified: '2017-02-09 16:15'
   }
 /*
   {
