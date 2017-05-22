d222b301e6abb4d7a8ce1becf5231bc5247e3544
wangshaojing <wangshaojing@cmcm.com>
2017-05-22 15:52:51 +0800

    update cm 2dcode, make index for new cms

---
+++ cms/json-data.php
@@ -0,0 +1,76 @@
+<?php
+$lang = "en-us";
+$title = "CM Security - #1 AntiVirus & Privacy Protection App";
+$description = "The best security app to protect your device. With the # 1 antivirus engine inside, it provides AppLock with fingerprint support, Safe Browsing to secure privacy, and Junk Clean to boost devices.";
+$keywords = "antivirus, CM security, security, CM, applock, privacy, phone boost, booster, boost, clean, trojan, lock";
+
+$fbTitle = "CM Security - #1 AntiVirus & Privacy Protection App";
+$fbDescription = "The best security app to protect your device. With the # 1 antivirus engine inside, it provides AppLock with fingerprint support, Safe Browsing to secure privacy, and Junk Clean to boost devices.";
+$fbImgUrl = "http://www.cmcm.com/images/share/cms-share.jpg";
+$siteName = "CM Security";
+
+$twTitle = "CM Security - #1 AntiVirus & Privacy Protection App";
+$twDescription = "The best security app to protect your device. With the # 1 antivirus engine inside, it provides AppLock with fingerprint support, Safe Browsing to secure privacy, and Junk Clean to boost devices.";
+$twImgUrl = "http://www.cmcm.com/images/share/cms-share.jpg";
+$twImgAlt = "CM Security AppLock AntiVirus";
+
+$iOSName = "CM Security";
+$iOSId = "1111447873";
+$iOSScheme = "cms://";
+$AndName = "CM Security";
+$AndScheme = "cmsecuritylink://default/1000/";
+$AndPackage = "com.cleanmaster.security";
+?>
+
+[!--temp.newPublicBase--]
+[!--temp.public.static--]
+<link rel="alternate" href="http://www.cmcm.com/es-es/cm-security/" hreflang="es-ES" />
+<link rel="alternate" href="http://www.cmcm.com/fr-fr/cm-security/" hreflang="fr-FR" />
+<link rel="alternate" href="http://www.cmcm.com/ja-jp/cm-security/" hreflang="ja-JP" />
+<link rel="alternate" href="http://www.cmcm.com/ko-kr/cm-security/" hreflang="ko-KR" />
+<link rel="alternate" href="http://www.cmcm.com/pt-pt/cm-security/" hreflang="pt-PT" />
+<link rel="alternate" href="http://www.cmcm.com/ru-ru/cm-security/" hreflang="ru-RU" />
+<link rel="alternate" href="http://www.cmcm.com/zh-cn/cm-security/" hreflang="zh-CN" />
+<link rel="alternate" href="http://www.cmcm.com/zh-tw/cm-security/" hreflang="zh-TW" />
+<link rel="stylesheet" href="/css/v4.cms.css" type="text/css" />
+<style>
+#menus .cms { color:#fff;}
+</style>
+</head>
+<body class="bodyCMS productFooter">
+<div id="wrap">
+[!--temp.en-us.menu--]
+
+<?php
+$bigTitle = "CM Security";
+$subTitle = "#1 AntiVirus & Privacy Protection";
+$downloadOn = "Download on";
+$downloadTxt = "Please download it with an Android device or iPhone";
+
+$facebookUrl = "https://www.facebook.com/kscms?fref=ts";
+$googlePlusUrl = "https://plus.google.com/108470700304320526925/";
+$twitterUrl = "https://twitter.com/Cm_Security";
+
+$appLockTitle = "Applock";
+$appLockDesc = "Protect apps from snoopers";
+$appLockEntry = "AppLock protects apps that are important to you. Also, it catches intruders who try to unlock protected apps and notifies you with intruders’photos.";
+
+$antiVirusTitle = "Anti-Virus";
+$antiVirusDesc = "Keep device safe from viruses, vulnerabilities, and spyware";
+$antiVirusEntry = "Anti-Virus Engine has been ranked #1 by AV-TEST repeatedly. It helps you detect and remove viruses, vulnerabilities, and spywares with one click.";
+
+$privacyTitle = "Privacy Cleaner";
+$privacyDesc = "Keep personal information safe";
+$privacyEntry = "Privacy Cleaner erases your personal information in clipboard and browsing records, including on-line videos, shopping, and banking.";
+
+$wifiScannerTitle = "Wi-Fi Scanner";
+$wifiScannerDesc = "Makes Wi-Fi connection not only safe but also faster";
+$wifiScannerEntry = "Wi-Fi Scanner identifies Wi-Fi phishing scams. Also, it helps you monitor & boost Wi-Fi speed.";
+?>
+
+[!--temp.CMSTemp--]
+[!--temp.en-us.footer.pro--]
+</div>
+[!--temp.public.footjs--]
+</body>
+</html>
