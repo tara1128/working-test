d7a7cf8ceaa91d71453e0f50ccd643208dcfc457
wangshaojing <wangshaojing@cmcm.com>
2017-05-23 11:42:18 +0800

    Add pmp

--- pmp/datas/hr-management/json.js
+++ pmp/datas/hr-management/json.js
@@ -20,19 +20,23 @@ datas: {
         {text: '事业环境因素和组织过程资产'}
       ],
       tools: [
-        {text: ''},
-        {text: ''},
+        {text: '组织图和职位描述', ext: [
+          {text: '确保每个工作包有明确责任人'},
+          {text: '确保全体成员都知道自己的角色和职责'},
+          {text: '层级型（Hierarchical-type charts）：组织分解结构（Organizational Breakdown Structure），规定高层级角色（组织结构按部门划分，在每个部门下列出项目活动和工作包）'},
+          {text: '矩阵型（Matrix-based charts）：责任分配矩阵RAM（Responsibility Assignments Matrix，也叫责任矩阵，显示分配给每个工作包的资源，定义每个人做什么事，每个事哪些人来做。即“人人有事做，事事有人做”。可以在多个层次上分配RAM，高层次上分配每个小组负责WBS哪个部分，低层次上分配每个人负责哪个活动。）注意：确保任何一个活动只有一个人负责Accountable，RACI中，R（Responsible执行）、A（Accountable负责）、C（Consult咨询）、I（Inform），其中A是老大，每个活动确定有且仅有一个A！'},
+          {text: '文本型（用文字描述记录详细职责）'},
+        ]},
+        {text: '人际交往'},
         {text: '组织理论'},
         {text: '专家判断'},
         {text: '会议'}
       ],
       outputs: [
         {text: '人力资源管理计划', ext: [
-          {text: ''},
-          {text: ''},
-          {text: ''},
-          {text: ''},
-          {text: ''},
+          {text: 'Human Resource Management Plan，定义、配备、管理、控制以及遣散项目人力资源的指南'},
+          {text: '1. 角色和职责：Role、Responsibility、Competency、Authority'},
+          {text: '2. 人员配备管理计划Staffing management plan：在何时，以何种方式，获得团队成员，以及他们将工作多久'},
           {text: ''}
         ]}
       ]
