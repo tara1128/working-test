a34372a91c29224543526f268d9ddd3dc73681e4
wangshaojing <wangshaojing@cmcm.com>
2017-06-19 18:20:43 +0800

    Fix some bugs for pc and mobiles of PG

---
+++ photo-grid/langs.js
@@ -0,0 +1,117 @@
+/*
+  Multiple languages for PhotoGrid
+  Starts from en-us
+  Latest modified: 2017-06-19 11:52
+*/
+
+/* ===== en-us ===== */
+/* 第1屏: */
+var openTitle = "PhotoGrid: The Best Collage Maker &amp; Photo Sharing Tool";
+
+/* 第2屏: */
+var secTitle = "Spectacular Photo Editing Artifacts";
+var secSubtitle = "PhotoGrid is not only the most Popular photo collage maker, but also a Creative community. Explore PhotoGrid community and enjoy photo-editing Fun right now.";
+var secImgComment1 = "PhotoGrid is the best Collage Maker.";
+var secImgComment2 = "PhotoGrid is a Creative Community.";
+var secVideoDescr = "Video introduction";
+
+/* 第3屏: */
+var thrName1 = "Layout";
+var thrDesc1 = "300+ Layouts &amp; Daily Templates";
+var thrText1 = "The best and easiest way to make collages with PhotoGrid - make your pics colorful in moments.";
+
+var thrName2 = "Wow! Filter";
+var thrDesc2 = "60+ Animated Filter Effects";
+var thrText2 = "Special effects camera for selfies or video recording. Make ordinary photos look fantastic with 60+ animated filter effects!";
+
+var thrName3 = "Filter";
+var thrDesc3 = "50+ Beautiful Filters";
+var thrText3 = "Add one of over 50 filter effects to enhance the mood, including selfie, vintage,scenery, halo, black and white, festival and more.";
+
+var thrName4 = "Sticker";
+var thrDesc4 = "500+ Cute Sticker";
+var thrText4 = "Personalize your photo collage with 500+ stickers and cute emoji";
+
+var thrName5 = "Text";
+var thrDesc5 = "Stylish Fonts &amp; Colors";
+var thrText5 = "Supports various types of font styles and colors. Write some words, let the photo tell your story.";
+
+var thrName6 = "Crop &amp; Resize";
+var thrDesc6 = "Horizontal &amp; Vertical Cropping";
+var thrText6 = "Supports horizontal and vertical cropping &amp; resizing. Crop &amp; Resize your images in different aspect ratio.";
+
+
+
+/* 第4屏: */
+var fthTitle = "Unique Features";
+var fthDescr = "Create wonderful effects with any combination of stunning photographs by using PhotoGrid's easy to follow steps. Completely free!";
+
+var fthIcon1 = "Grid";
+var fthText1 = "Turn pics to a wonderful collage with multitudinous layouts.";
+
+var fthIcon2 = "Editor";
+var fthText2 = "Stylize your shots with perfectly categorized filter effects.";
+
+var fthIcon3 = "Venus Filter";
+var fthText3 = "Enjoy selfie with filter Camera. Support Retouch feature.";
+
+var fthIcon4 = "Twinkle";
+var fthText4 = "Turn your photos into stunning video stories.";
+
+var fthIcon5 = "Scrapbook";
+var fthText5 = "Pin treasured moments to your personal board.";
+
+var fthIcon6 = "Slide Show";
+var fthText6 = "Turn your photos into stunning video stories.";
+
+var fthIcon7 = "Filmstrip";
+var fthText7 = "Tell a story via a long picture, combining multiple photos.";
+
+var fthIcon8 = "Poster";
+var fthText8 = "Style your images just like magazine covers. Daily update.";
+
+
+
+/* 第5屏: */
+var fifTitle = "Creative Community";
+var fifSubtitle = "Create, Share And Discover Something Fun On PhotoGrid.";
+
+var fifItem1 = "Share";
+var fifText1 = "Join PhotoGrid Community to share the wonderful moments.";
+
+var fifItem2 = "Interact";
+var fifText2 = "Engage with friends and users around the world by tracking, comments and likes.";
+
+var fifItem3 = "Explore";
+var fifText3 = "Explore celebrity fashion, travel, food and other popular works.";
+
+var fifItem4 = "Activities";
+var fifText4 = "Get featured and become famous.";
+
+
+/* 第6屏: */
+var sixSubtitle = "Download the most magical photo-editor, Create your PhotoGrid photo in just one tap!";
+
+
+
+
+/* ===== es-es ===== */
+
+
+
+/* ===== fr-fr ===== */
+
+
+
+/* ===== ru-ru ===== */
+
+
+
+/* ===== pt-pt ===== */
+
+
+
+/* ===== ko-kr ===== */
+/* ===== ja-jp ===== */
+/* ===== zh-tw ===== */
+/* ===== zh-cn ===== */
